62906fdc8669355b3a6109c88d9249d6c6e33158
williamowen <williamowen>
2015-05-19 09:35:17 -0700

    add index.js geometry modules and app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+var geometry = require('./index.js');
+console.log( 'The area of a circle of radius 4 is '
+           + geometry.circlearea(4));
+
+console.log( 'The circumference of a circle of radius 4 is'
+           , geometry.circlecircumference(4));
